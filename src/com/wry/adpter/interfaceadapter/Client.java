0906ee2dc97f5d5dad95ef81d388f0b7001560d1
Mi <wangruiyu1314521@outlook.com>
2019-09-10 09:20:59 +0800

    王瑞玉-20190803

---
+++ src/com/wry/adpter/interfaceadapter/Client.java
@@ -0,0 +1,13 @@
+package com.wry.adpter.interfaceadapter;
+
+public class Client {
+    public static void main(String[] args) {
+        new AbsAdapter(){
+            @Override
+            public void method1() {
+                //实现需要适配的方法
+                System.out.println("使用了M1 的方法！");
+            }
+        }.method1();
+    }
+}
---
+++ src/com/wry/adpter/interfaceadapter/InterfaceClass.java
@@ -0,0 +1,12 @@
+package com.wry.adpter.interfaceadapter;
+
+/**
+ * 接口类，有3个抽象方法
+ */
+public interface InterfaceClass {
+    void method1();
+
+    void method2();
+
+    void method3();
+}
