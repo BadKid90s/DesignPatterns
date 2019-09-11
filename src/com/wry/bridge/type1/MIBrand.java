df91040f0cdbb056d5690a87fe8f5068ccbfef00
Mi <wangruiyu1314521@outlook.com>
2019-09-11 14:36:54 +0800

    Mr.Wang

---
+++ src/com/wry/bridge/type1/MIBrand.java
@@ -0,0 +1,23 @@
+package com.wry.bridge.type1;
+
+public class MIBrand implements Brand {
+
+    private String name = "小米手机";
+
+    @Override
+    public void open() {
+        System.out.println(name + "开机~~~");
+    }
+
+    @Override
+    public void close() {
+        System.out.println(name + "关机~~~");
+
+    }
+
+    @Override
+    public void call() {
+        System.out.println(name + "打电话··~~~");
+
+    }
+}
---
+++ src/com/wry/bridge/type2/DrawAPI.java
@@ -0,0 +1,5 @@
+package com.wry.bridge.type2;
+
+public interface DrawAPI {
+    void drawCircle(int radius, int x, int y);
+}
