e258177cbbca3de764ce2edd499070552906f440
Mi <wangruiyu1314521@outlook.com>
2020-01-02 16:54:55 +0800

    建造者模式

--- src/com/wry/builder/type3/Client.java
+++ src/com/wry/builder/type3/Client.java
@@ -5,16 +5,15 @@ import com.wry.utils.XMLUtil;
 
 public class Client {
     public static void main(String[] args) {
-        ActorBuilder builder=new DevilBuilder();
-        ActorController controller=new ActorController();
+        ActorBuilder builder = new DevilBuilder();
+        ActorController controller = new ActorController();
         Actor actor = controller.construct(builder);
         System.out.println(actor.toString());
 
         System.out.println("--------------------使用配置文件获取---------------------");
         //读取配置文件中的参数
-        builder= (ActorBuilder)XMLUtil.getBean(new Client(), "className");
+        builder = (ActorBuilder) XMLUtil.getBean(new Client(), "className");
         actor = controller.construct(builder);
         System.out.println(actor.toString());
-
     }
 }
